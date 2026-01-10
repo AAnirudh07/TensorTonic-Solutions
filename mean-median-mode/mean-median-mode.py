2c431b336a55e7be1726d6059c59aee578c431c9
AAnirudh07 <86918353+AAnirudh07@users.noreply.github.com>
2026-01-10 09:22:36 +0530

    Add solution for mean-median-mode

---
+++ mean-median-mode/mean-median-mode.py
@@ -0,0 +1,25 @@
+import numpy as np
+from collections import Counter
+
+def mean_median_mode(x):
+    """
+    Compute mean, median, and mode.
+    """
+    mean = sum(x) / len(x)
+    
+    median = None
+    if len(x)%2 == 0:
+        median = (x[len(x)//2] + x[(len(x)//2) - 1]) / 2
+    else:
+        median = x[len(x)//2] 
+
+    counts = Counter(sorted(x))
+    mode = None
+    freq = -1
+
+    for item, count in counts.items():
+        if count > freq or (count == freq and item < mode):
+            mode = item
+            freq = count
+
+    return float(mean), float(median), float(mode)
