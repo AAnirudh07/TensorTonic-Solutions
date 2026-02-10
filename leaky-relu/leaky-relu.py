0833a5557891b85d1f5ed56bb974261dcf03c153
AAnirudh07 <86918353+AAnirudh07@users.noreply.github.com>
2026-02-10 09:25:04 +0530

    Update solution for leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -5,4 +5,5 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     x = np.atleast_1d(x)
-    return np.maximum(x, 0.0) + alpha*np.minimum(x, 0.0)
+    # return np.maximum(x, 0.0) + alpha*np.minimum(x, 0.0)
+    return np.where(x>0, x, alpha*x)
